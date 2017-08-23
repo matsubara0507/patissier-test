a3700dc96ec43789ef9aa64eb50005385abab1b4
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2017-08-23 19:36:24 +0900

    Remove debug code

--- web/controllers/branches_controller.ex
+++ web/controllers/branches_controller.ex
@@ -3,7 +3,6 @@ defmodule PastryChefTest.BranchesController do
 
   def index(conn, _params) do
     token = Application.get_env(:pastry_chef_test, PastryChefTest.BranchesController)[:github_auth_token]
-    IO.puts "token: #{token}"
     client = Tentacat.Client.new(%{access_token: token})
 
     branches = Tentacat.Repositories.Branches.list "elixir-lang", "elixir", client
