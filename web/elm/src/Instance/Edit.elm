45cc6baf1e9d2f5a9c9a3dbc9f6015777be68ea3
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2017-09-06 15:58:03 +0900

    Refactor Button actions

--- web/elm/src/Instance/Edit.elm
+++ web/elm/src/Instance/Edit.elm
@@ -27,16 +27,28 @@ type alias Model =
   , result : RemoteData String String
   }
 
+type ButtonAction
+  = Rename String
+  | Deploy (List (RepoName, String))
+  | Terminate
+
 type Msg
   = Name String
-  | Rename String String
-  | Terminate String
   | BranchSelector RepoName BS.Msg
+  | Push String ButtonAction
+  | FetchResult (Result Http.Error String)
   | FetchInstance (Result Http.Error Instance)
-  | FetchRename (Result Http.Error String)
-  | FetchTerminate (Result Http.Error String)
-  | FetchResultDeployEnv (Result Http.Error String)
-  | RequestToDeployEnv String (List (RepoName, String))
+
+-- type Msg
+--   = Name String
+--   | Rename String String
+--   | Terminate String
+--   | BranchSelector RepoName BS.Msg
+--   | FetchInstance (Result Http.Error Instance)
+--   | FetchRename (Result Http.Error String)
+--   | FetchTerminate (Result Http.Error String)
+--   | FetchResultDeployEnv (Result Http.Error String)
+--   | RequestToDeployEnv String (List (RepoName, String))
 
 init : Instance -> (Model, Cmd Msg)
 init = flip initModel model . .id
@@ -71,32 +83,21 @@ fetchInstance instanceId =
   in
     Http.send FetchInstance request
 
-fetchResult : String -> List (RepoName, String) -> Cmd Msg
-fetchResult instanceId branchNames =
-  let
-    apiUrl = "/api/instance/" ++ instanceId
-    body =
-      Http.multipartBody
-      <| List.map (uncurry Http.stringPart) branchNames
-    request = put apiUrl body string
-  in
-    Http.send FetchResultDeployEnv request
+fetchResult : String -> ButtonAction -> Cmd Msg
+fetchResult instanceId = Http.send FetchResult . toRequest instanceId
 
-fetchRename : String -> String -> Cmd Msg
-fetchRename instanceId instanceName =
-  let
-    apiUrl = "/api/instance/" ++ instanceId ++ "/rename/" ++ instanceName
-    request = put apiUrl Http.emptyBody string
-  in
-    Http.send FetchRename request
-
-fetchTerminate : String -> Cmd Msg
-fetchTerminate instanceId =
-  let
-    apiUrl = "/api/instance/" ++ instanceId
-    request = delete apiUrl string
-  in
-    Http.send FetchTerminate request
+toRequest : String -> ButtonAction -> Http.Request String
+toRequest instanceId action =
+  case action of
+    Rename name ->
+      put ("/api/instance/" ++ instanceId ++ "/rename/" ++ name) Http.emptyBody string
+    Deploy branchNames ->
+      let
+        body = Http.multipartBody $ List.map (uncurry Http.stringPart) branchNames
+      in
+        put ("/api/instance/" ++ instanceId) body string
+    Terminate ->
+      delete ("/api/instance/" ++ instanceId) string
 
 view : String -> Model -> Html Msg
 view instanceId model =
@@ -125,11 +126,7 @@ viewRename instanceId model =
                      , onInput Name
                      , defaultValue model.name
                      ] []
-             , button [ class "btn"
-                      , if not model.requesting then class "" else class "disabled"
-                      , onClick $ Rename instanceId model.name
-                      ]
-                      [ text "Rename" ]
+             , viewButton "Rename" "" True (Push instanceId $ Rename model.name) model
              ]
      ]
 
@@ -156,21 +153,24 @@ viewContent model =
             ]
     ]
 
+viewButton : String -> String -> Bool -> Msg -> Model -> Html Msg
+viewButton txt classTxt flag msg model =
+  button [ class "btn"
+         , class classTxt
+         , if not model.requesting && flag then class "" else class "disabled"
+         , onClick msg
+         ]
+         [ text txt ]
+
 viewDeployButton : String -> Model -> Html Msg
 viewDeployButton instanceId model =
   let
     repoToTuple repo = (repo.name, repo.branchModel.selectBranchName)
     flag = List.foldl (&&) True . (::) (model.name /= "")
          $ List.map (\repo -> repo.branchModel.selectBranchName /= "") model.repositories
+    msg = Push instanceId . Deploy $ List.map repoToTuple model.repositories
   in
-    div []
-      [ button [ class "btn btn-primary mt-2"
-               , if not model.requesting && flag then class "" else class "disabled"
-               , onClick . RequestToDeployEnv instanceId
-                 $ List.map repoToTuple model.repositories
-               ]
-               [ text "Deploy instance" ]
-      ]
+    div [] [ viewButton "Deploy instance" "btn-primary mt-2" flag msg model ]
 
 viewControlState : String -> Model -> Html Msg
 viewControlState instanceId model =
@@ -184,9 +184,7 @@ viewControlState instanceId model =
 viewTerminate : String -> Model -> Html Msg
 viewTerminate instanceId model =
   li [ class "Box-row one-half" ]
-     [ button [ class "btn btn-danger boxed-action"
-              , onClick $ Terminate instanceId ]
-              [ text "Terminate" ]
+     [ viewButton "Terminate" "btn-danger boxed-action" True (Push instanceId Terminate) model
      , strong [] [ text "Terminate Instance" ]
      ]
 
@@ -221,9 +219,9 @@ viewState state =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Name name -> ({ model | requesting = True, name = name }, Cmd.none)
-    Rename instanceId name -> (model, fetchRename instanceId name)
-    Terminate instanceId -> (model, fetchTerminate instanceId)
+    Name name -> ({ model | name = name }, Cmd.none)
+    -- Rename instanceId name -> ({ model | requesting = True }, fetchRename instanceId name)
+    -- Terminate instanceId -> (model, fetchTerminate instanceId)
     BranchSelector repoName branchMsg ->
       case List.find (\repo -> repo.name == repoName) model.repositories of
         Just repo ->
@@ -231,23 +229,17 @@ update msg model =
           $ BS.update branchMsg repo.branchModel
         Nothing ->
           ({ model | result = Failure "repo is not found..." }, Cmd.none)
+    Push instanceId action ->
+      ({ model | requesting = True }, fetchResult instanceId action)
     FetchInstance (Ok instance) -> (updateInstance instance model, Cmd.none)
     FetchInstance (Err error) ->
       ({ model | instance = Failure "Something went wrong..." }, Cmd.none)
-    FetchRename (Ok response) ->
+    FetchResult (Ok response) ->
       ({ model | requesting = False, result = Success response }, Cmd.none)
-    FetchRename (Err error) ->
+    FetchResult (Err error) ->
       ({ model | requesting = False, result = Failure "Something went wrong..." }, Cmd.none)
-    FetchTerminate (Ok response) ->
-      ({ model | requesting = False, result = Success response }, Cmd.none)
-    FetchTerminate (Err error) ->
-      ({ model | requesting = False, result = Failure "Something went wrong..." }, Cmd.none)
-    FetchResultDeployEnv (Ok response) ->
-      ({ model | requesting = False, result = Success response }, Cmd.none)
-    FetchResultDeployEnv (Err error) ->
-      ({ model | requesting = False, result = Failure "Something went wrong..." }, Cmd.none)
-    RequestToDeployEnv instanceId branches ->
-      ({ model | requesting = True }, fetchResult instanceId branches)
+    -- RequestToDeployEnv instanceId branches ->
+    --   ({ model | requesting = True }, fetchResult instanceId branches)
 
 updateRepo : RepoName -> BS.Model -> Model -> Model
 updateRepo repoName branchModel model =
